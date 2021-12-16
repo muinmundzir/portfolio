d03663ace64f5a3a99770a5d54a22cf880a163fb
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-12-16 15:07:33 +0700

    style: edit default display

--- components/Dots.js
+++ components/Dots.js
@@ -2,7 +2,7 @@ const Dots = () => {
   return (
     <>
       {/* Dots */}
-      <div className="flex items-center justify-between gap-3 w-ful">
+      <div className="hidden md:flex items-center justify-between gap-3 w-ful">
         <div className="bg-primary/60 h-3 w-4"></div>
         <div className="bg-primary/60 h-3 w-4"></div>
         <div className="bg-primary/60 h-3 w-4"></div>
