f8c63c39eb50a11e4d15ddb5825a718faa05bf50
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-12-16 14:44:31 +0700

    add: component

---
+++ components/Dots.js
@@ -0,0 +1,19 @@
+const Dots = () => {
+  return (
+    <>
+      {/* Dots */}
+      <div className="flex items-center justify-between gap-3 w-ful">
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-primary/60 h-3 w-4"></div>
+        <div className="bg-gradient-to-r from-primary/60 to-primary/0 flex-grow h-3 w-4"></div>
+      </div>
+    </>
+  )
+}
+
+export default Dots
