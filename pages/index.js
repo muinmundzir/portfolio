8ce25d8a79bc4facb74d10349ffbc1b590cfc7e8
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-12-16 11:17:31 +0700

    add: icons

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1>Let&apos;s get started!</h1>;
+  return <></>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem',
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#00ADB5',
+          black: '#222831',
+          gray: '#393E46',
+          white: '#EEEEEE',
+        },
+      },
+    },
   },
   plugins: [],
 };
